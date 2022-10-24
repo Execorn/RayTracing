077091b0a5209a64ad297de354d8249f330a5f14
Execorn <n.biryukov.04@gmail.com>
2022-10-24 10:21:51 +0300

    spheres added

--- src/main.cpp
+++ src/main.cpp
@@ -24,24 +24,30 @@ int main() {
     ray_tracing_scene.addLightSource({-20,  20,  20});
     ray_tracing_scene.addLightSource({ 30,  50, -25});
     ray_tracing_scene.addLightSource({ 30,  20,  30});
+    ray_tracing_scene.addLightSource({ -20, -20,   20});
 
     ray_tracing_scene.addMaterial("ivory",      {1.0, {0.9,  0.5, 0.1, 0.0}, {0.4, 0.4, 0.3},   50.});       
     ray_tracing_scene.addMaterial("glass",      {1.5, {0.0,  0.9, 0.1, 0.8}, {0.6, 0.7, 0.8},  125.});
     ray_tracing_scene.addMaterial("red_rubber", {1.0, {1.4,  0.3, 0.0, 0.0}, {0.3, 0.1, 0.1},   10.});
     ray_tracing_scene.addMaterial("mirror",     {1.0, {0.0, 16.0, 0.8, 0.0}, {1.0, 1.0, 1.0}, 1425.});
+    ray_tracing_scene.addMaterial("cringe1",    {1.0, {1.4,  0.3, 0.1, 0.0}, {1.0, 0.41, 0.705}, 250.});
 
     TMaterial_ orange_material;
     orange_material.diffuse_color_ = {0.3, 0.2, 0.1};
     ray_tracing_scene.addMaterial("orange", orange_material);
 
-    ray_tracing_scene.addSphere({{-3,    0,   -16}, 2, {}}, "ivory");
+    ray_tracing_scene.addSphere({{-3,    0,   -16}, 2,  {}}, "ivory");
     ray_tracing_scene.addSphere({{-1.0, -1.5, -12}, 2, {}}, "glass");
-    ray_tracing_scene.addSphere({{ 1.5, -0.5, -18}, 3, {}}, "red_rubber");
-    ray_tracing_scene.addSphere({{ 7,    5,   -18}, 4, {}}, "mirror");
+    ray_tracing_scene.addSphere({{ 1.5, -0.5, -18}, 3,  {}}, "red_rubber");
+    ray_tracing_scene.addSphere({{-7  ,    4, -18}, 4,  {}}, "mirror");
+    ray_tracing_scene.addSphere({{ 7  ,   11, -18}, 7, {}}, "mirror");
+    ray_tracing_scene.addSphere({{5  , -2.5  , -10},      2, {}}, "cringe1");
 
     ray_tracing_scene.addPlane({0, 1, 0, 4, true}, "orange");
     ray_tracing_scene.addPlane({0, 0, 1, 100, true}, "red_rubber");
     ray_tracing_scene.addPlane({0, 0, 1, -31, true}, "ivory");
+
+    bool isRendered = false;
 
     while (window.isOpen()) {
         TEvent_ main_event;
@@ -50,14 +56,17 @@ int main() {
 				window.close();
             }
         }
+        if (!isRendered) {
+            ray_tracing_scene.trace_scene(TRACER_WIDTH, TRACER_HEIGHT, PI / 3.0, vector_arr, vertex_arr);
 
-        ray_tracing_scene.trace_scene(TRACER_WIDTH, TRACER_HEIGHT, PI / 3.0, vector_arr, vertex_arr);
+            TColor_ background(0, 0, 0);
+            window.clear(background);
+            window.render(vertex_arr, TRACER_WIDTH * TRACER_HEIGHT);
 
-        TColor_ background(0, 0, 0);
-        window.clear(background);
-        window.render(vertex_arr, TRACER_WIDTH * TRACER_HEIGHT);
+            window.display();
 
-        window.display();
+            isRendered = true;
+        }
     }
 
     delete[] vertex_arr;
